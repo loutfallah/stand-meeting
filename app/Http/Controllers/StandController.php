857f6b115d3f23f4ea57404eddf9e213348798c3
loutfallah <loutfallah.25@gmail.com>
2020-06-03 13:10:40 +0100

    first commit

---
+++ app/Http/Controllers/StandController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Stand;
+use Illuminate\Http\Request;
+
+class StandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $stand = Stand::with('reseau','video','lienExtern','galerie','temoignage','Document')->get();
+        /* $stand = Stand::all(); */
+
+        return $stand ;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $stand = new Stand();
+        $stand->description = $request->description;
+        $stand->status =  $request->status;
+        $stand->theme_id =  $request->theme_id;
+        $stand->espace_exposition_id = $request->espace_exposition_id;
+        $stand->save();
+        return $stand ;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $stand = Stand::with('reseau','video','lienExtern','galerie','temoignage','Document')->find($id);
+        return $stand;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $stand = Stand::findOrFail($id);
+        $stand->description = $request->description;
+        $stand->status =  $request->status;
+        $stand->theme_id =  $request->theme_id;
+        $stand->espace_exposition_id = $request->espace_exposition_id;
+        $stand->save();
+        return $stand ;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Stand::destroy($id);
+        return 'deleted';
+    }
+}
---
+++ app/Stand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stand extends Model
+{
+    
+    protected $fillable = [ 'id', 'description', 'status','theme_id', 'espace_exposition_id'];
+
+    public function theme() {
+        return $this->belongsTo('App\theme');
+    }
+
+    public function reseau() {
+        return $this->hasMany('App\Reseaux_sociaux');
+    }
+
+    public function video() {
+        return $this->hasMany('App\Video');
+    }
+    public function lienExtern() {
+        return $this->hasMany('App\Lien_extern');
+    }
+    public function galerie() {
+        return $this->hasMany('App\Galerie');
+    }
+    public function temoignage() {
+        return $this->hasMany('App\Temoignage');
+    }
+    public function document() {
+        return $this->hasMany('App\Document');
+    }
+    public function espaceExposition() {
+        return $this->belongsTo('App\Espace_exposition');
+    }
+}
